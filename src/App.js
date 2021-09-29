7a7345ed03f6b6f5b33299c8a9d428777615cc49
Ilwad Abukor <ilwadabukor@hotmail.com>
2021-09-29 22:42:11 +0100

    least changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
-import './App.css';
-
+import "./App.css";
+import Tablet from "./Tablet";
+import Data from "./data";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img className="bg-header" src="./images/bg-header-desktop.svg" />
       </header>
+      <div className="tablets">
+        {/* <Tablet
+          NEW={true}
+          FEATURE={true}
+          logo={"./images/photosnap.svg"}
+          company={"Photosnap"}
+          position={"Senior Frontend Developer"}
+          postedAt={"1d ago"}
+          contract={"Full Time"}
+          location={"Remote"}
+          role={"Frontend"}
+          level={"Senior"}
+          languages={["HTML", "CSS", "JavaScript"]}
+          tools={[]}
+        />
+        <Tablet
+          NEW={true}
+          FEATURE={true}
+          logo={"./images/photosnap.svg"}
+          company={"Photosnap"}
+          position={"Senior Frontend Developer"}
+          postedAt={"1d ago"}
+          contract={"Full Time"}
+          location={"Remote"}
+          role={"Frontend"}
+          level={"Senior"}
+          languages={["HTML", "CSS", "JavaScript"]}
+          tools={[]}
+        /> */}
+         {Data.map((item) => (
+          <Tablet
+            key={item.id}
+            NEW={item.new}
+            FEATURED={item.featured}
+            logo={item.logo}
+            company={item.company}
+            position={item.position}
+            postedAt={item.postedAt}
+            contract={item.contract}
+            location={item.location}
+            role={item.role}
+            level={item.level}
+            languages={item.languages}
+            tools={item.tools}
+          />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/Tablet.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./tablet.css";
+
+function Tablet({
+  NEW,
+  FEATURED,
+  logo,
+  company,
+  position,
+  postedAt,
+  contract,
+  location,
+  role,
+  level,
+  languages,
+  tools
+}) {
+  return (
+    <div className="wrapper">
+      <div className="left">
+        <div className="info">
+          <img className="logo" src={logo} />
+          <div className="text">
+            <div className="top">
+              <div className="company">{company}</div>
+              {NEW && <div className="new status">NEW!</div>}
+              {FEATURED && <div className="feature status">feature</div>}
+            </div>
+            <div className="position">{position}</div>
+            <div className="bottom">
+              <p>{postedAt}</p>
+              <p>•</p>
+              <p>{contract}</p>
+              <p>•</p>
+              <p>{location}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="right">
+        <div className="tag">{role}</div>
+        <div className="tag">{level}</div>
+        {languages.map((language) => (
+          <div className="tag">{language}</div>
+        ))}
+        {tools.map((tool) => (
+          <div className="tag">{tool}</div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Tablet;
