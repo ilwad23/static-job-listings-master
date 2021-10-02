58f1030658a2d5305c16e0fadb5a22fd9417d8fa
Ilwad Abukor <ilwadabukor@hotmail.com>
2021-10-02 21:04:54 +0100

    `

--- src/App.js
+++ src/App.js
@@ -1,58 +1,48 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Tablet from "./Tablet";
 import Data from "./data";
+
+const useScreenSize = () => {
+  const [size, setSize] = useState(window.innerWidth);
+  useEffect(() => {
+    const handleResize = () => {
+      setSize(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+  }, []);
+  return size > 700 ? "desktop" : "mobile";
+};
+
 function App() {
+  const desktopOrMobile = useScreenSize();
+  const filteredData = Data.filter((item) => item);
+  const allData = filteredData.map((item) => (
+    <Tablet
+      key={item.id}
+      NEW={item.new}
+      FEATURED={item.featured}
+      logo={item.logo}
+      company={item.company}
+      position={item.position}
+      postedAt={item.postedAt}
+      contract={item.contract}
+      location={item.location}
+      role={item.role}
+      level={item.level}
+      languages={item.languages}
+      tools={item.tools}
+    />
+  ));
+  console.log(filteredData);
   return (
     <div className="App">
       <header className="App-header">
-        <img className="bg-header" src="./images/bg-header-desktop.svg" />
+        <img className="bg-header" src={`./images/bg-header-${desktopOrMobile}.svg`} />
       </header>
       <div className="tablets">
-        {/* <Tablet
-          NEW={true}
-          FEATURE={true}
-          logo={"./images/photosnap.svg"}
-          company={"Photosnap"}
-          position={"Senior Frontend Developer"}
-          postedAt={"1d ago"}
-          contract={"Full Time"}
-          location={"Remote"}
-          role={"Frontend"}
-          level={"Senior"}
-          languages={["HTML", "CSS", "JavaScript"]}
-          tools={[]}
-        />
-        <Tablet
-          NEW={true}
-          FEATURE={true}
-          logo={"./images/photosnap.svg"}
-          company={"Photosnap"}
-          position={"Senior Frontend Developer"}
-          postedAt={"1d ago"}
-          contract={"Full Time"}
-          location={"Remote"}
-          role={"Frontend"}
-          level={"Senior"}
-          languages={["HTML", "CSS", "JavaScript"]}
-          tools={[]}
-        /> */}
-         {Data.map((item) => (
-          <Tablet
-            key={item.id}
-            NEW={item.new}
-            FEATURED={item.featured}
-            logo={item.logo}
-            company={item.company}
-            position={item.position}
-            postedAt={item.postedAt}
-            contract={item.contract}
-            location={item.location}
-            role={item.role}
-            level={item.level}
-            languages={item.languages}
-            tools={item.tools}
-          />
-        ))}
+        {/* <Fi */}
+        {allData}
       </div>
     </div>
   );
--- src/Tablet.js
+++ src/Tablet.js
@@ -13,41 +13,45 @@ function Tablet({
   role,
   level,
   languages,
-  tools
+  tools,
 }) {
   return (
-    <div className="wrapper">
-      <div className="left">
-        <div className="info">
-          <img className="logo" src={logo} />
-          <div className="text">
-            <div className="top">
-              <div className="company">{company}</div>
-              {NEW && <div className="new status">NEW!</div>}
-              {FEATURED && <div className="feature status">feature</div>}
-            </div>
-            <div className="position">{position}</div>
-            <div className="bottom">
-              <p>{postedAt}</p>
-              <p>•</p>
-              <p>{contract}</p>
-              <p>•</p>
-              <p>{location}</p>
+    <>
+      <div className="filter"></div>
+      <div className="wrapper tablet">
+        {FEATURED && <div className="rec"></div>}
+        <div className="left">
+          <div className="info">
+            <img className="logo" src={logo} />
+            <div className="text">
+              <div className="top">
+                <div className="company">{company}</div>
+                {NEW && <div className="new status">NEW!</div>}
+                {FEATURED && <div className="feature status">feature</div>}
+              </div>
+              <div className="position">{position}</div>
+              <div className="bottom">
+                <p>{postedAt + " "}</p>
+                <p>•</p>
+                <p>{" " + contract + " "}</p>
+                <p>•</p>
+                <p>{" " + location}</p>
+              </div>
             </div>
           </div>
         </div>
+        <div className="right">
+          <div className="tag">{role}</div>
+          <div className="tag">{level}</div>
+          {languages.map((language) => (
+            <div className="tag">{language}</div>
+          ))}
+          {tools.map((tool) => (
+            <div className="tag">{tool}</div>
+          ))}
+        </div>
       </div>
-      <div className="right">
-        <div className="tag">{role}</div>
-        <div className="tag">{level}</div>
-        {languages.map((language) => (
-          <div className="tag">{language}</div>
-        ))}
-        {tools.map((tool) => (
-          <div className="tag">{tool}</div>
-        ))}
-      </div>
-    </div>
+    </>
   );
 }
 
